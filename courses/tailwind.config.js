47a0e6ce4530be8fa79af0a38312dd53a8d48fa7
Mohannad Moujib <mohannadmoujiib@gmail.com>
2024-10-19 00:19:50 +0100

    1st commit

---
+++ courses/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    // Add any other directories where you have components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
