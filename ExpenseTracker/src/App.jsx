d595f75e6b4e5ac520465d435733bcc3635e0325
Mohannad Moujib <mohannadmoujiib@gmail.com>
2024-11-06 23:19:24 +0100

    1st commit

---
+++ ExpenseTracker/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Home from './comps/home'
+
+const App = () => {
+  return (
+    <Home />
+  )
+}
+
+export default App
---
+++ ExpenseTracker/src/comps/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <h1>Expense Tracker</h1>
+  )
+}
+
+export default Home
